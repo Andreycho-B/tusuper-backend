47503bc33914fd2e55df3e1217f8062649264426
unknown <jhojansaa0528@gmail.com>
2026-03-18 15:55:48 -0500

    moduloUsuariosTuSuper

--- src/users/interfaces/user.ts
+++ src/users/interfaces/user.ts
@@ -1,9 +1,13 @@
-import { Role } from "src/roles/entities/role.entity";
+import { Role } from '../../roles/entities/role.entity';
 
 export interface UserModel {
-    id: number,
-    email: string,
-    password? : string,
-    isActive: boolean,
-    roles: Role[],
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password?: string;
+  isActive: boolean;
+  roles: Role[];
+  createdAt: Date;
+  updatedAt: Date;
 }

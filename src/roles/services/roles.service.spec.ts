4790f4176c68b4dca9188aadc7444c18a4254299
neita <dsantiagoneitagmail.com>
2026-04-13 16:35:09 -0500

    test: fix module resolution in roles service spec

--- src/roles/services/roles.service.spec.ts
+++ src/roles/services/roles.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { RolesService } from './services/roles.service';
+import { RolesService } from './roles.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Role } from '../entities/role.entity';
+import { ModulesService } from '../../modules/modules.service';
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RolesService],
+      providers: [
+        RolesService,
+        {
+          provide: getRepositoryToken(Role),
+          useValue: {},
+        },
+        {
+          provide: ModulesService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<RolesService>(RolesService);
